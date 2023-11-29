d46aeeb42202a46de40cd0f8af478a845879915a
Aleksei Briukhachev <abriukhachev@trask.cz>
2023-11-29 15:50:19 +0100

    Start js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// alert('Hello');
+
+// const { write } =sasds require('fs');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('18 years old', '18');
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt('What`s your name?', '');
+// answers[1] = prompt('What`s your surname?', '');
+// answers[2] = prompt('What`s your age?', '');
+
+// console.log(typeof(answers));
+
+const category = 'toys';
+
+// console.log('https://someurl.com/' + category + '/' + top);
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Hi, ${user}`);
