1fb180c8a188f9fad1af50c2d22451f492288d31
Aleksei Briukhachev <abriukhachev@trask.cz>
2023-11-30 01:15:41 +0100

    Functions

--- js/study.js
+++ js/study.js
@@ -1,5 +1,22 @@
-function showFirstMessage() {
-    console.log('Hello World!');
+let num = 20;
+function showFirstMessage(text) {
+    console.log(text);
+    num = 10;
 }
 
-showFirstMessage();
+showFirstMessage('Hello World!');
+console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+console.log(calc(4, 3));
+
+const logger = function() {
+    console.log('Hello');
+};
+logger();
+
+const calc = (a, b) => a + b;
+console.log(calc);
