4a275e1442229ef58006b06b3af9d4b9895e0b84
Aleksei Briukhachev <abriukhachev@trask.cz>
2024-02-18 23:47:04 +0100

    change components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,43 @@ import './App.css';
 import {Component} from "react";
 
 class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            position: ''
+        }
+        this.nextYear = this.nextYear.bind(this);
+    }
+
+    nextYear = () => {
+        console.log('+++');
+        this.setState({
+            years: this.state.years + 1
+        })
+    }
+
+    commitInputChanges = (e) => {
+        this.setState({
+            position: e.target.value
+        })
+    }
+
     render() {
         const {name, surname, link} = this.props;
+        const {position, years} = this.state;
         return (
             <div>
-                <h1>My name is {name}, surname - {surname}</h1>
+                <button onClick={this.nextYear}>+++</button>
+                <h1>My name is {name}, surname - {surname},
+                    age - {years},
+                    position - {position}
+                </h1>
                 <a href={link}>My profile</a>
+                <form>
+                    <span>Enter </span>
+                    <input type="text" onChange={this.commitInputChanges}/>
+                </form>
             </div>
         )
     }
