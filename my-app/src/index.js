437230d6e6e971c0397cb02bba11a615b79681c3
Aleksei Briukhachev <abriukhachev@trask.cz>
2024-02-22 23:58:55 +0100

    Finish React adv

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,4 @@
-import React, {StrictMode} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,9 +15,6 @@ const BigButton = styled(Button)`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
     <App />
-      {/*<BigButton as="a">Send report</BigButton>*/}
-  </StrictMode>
   //   elem
 );
